77eaa608cbed7101623ec7d8ef57a7042aac9a25
user <kosareva.karina2013@yandex.ru>
2023-02-21 23:27:31 +0300

    change utils.py

--- utils/utils.py
+++ utils/utils.py
@@ -15,3 +15,8 @@ class Stack:
         node.next_node = self.top
         self.top = node
 
+    def pop(self):
+        """Удаляет из стека верхний элемент и возвращает данные удаленного экземпляра класса Node"""
+        del_data = self.top
+        self.top = self.top.next_node
+        return del_data.data
