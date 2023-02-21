a2e63ed9224c9ee370e7e415bc6c92498f08fc4b
user <kosareva.karina2013@yandex.ru>
2023-02-21 23:27:38 +0300

    change test

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -30,3 +30,16 @@ class TestStack(unittest.TestCase):
         self.assertEqual(self.stack.top.next_node.data, 'data2')
         self.assertEqual(self.stack.top.next_node.next_node.data, 'data1')
         self.assertEqual(self.stack.top.next_node.next_node.next_node, None)
+
+    def test_stack_pop_add_data_one(self):
+        self.stack.push('data1')
+        data1 = self.stack.pop()
+        self.assertEqual(data1, 'data1')
+        self.assertEqual(self.stack.top, None)
+
+    def test_stack_pop_add_data_two(self):
+        self.stack.push('data1')
+        self.stack.push('data2')
+        data2 = self.stack.pop()
+        self.assertEqual(data2, 'data2')
+        self.assertEqual(self.stack.top.data, 'data1')
