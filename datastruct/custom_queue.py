0bd57e08e0f7fb202219f28edc640708cd548dd8
user <kosareva.karina2013@yandex.ru>
2023-03-08 03:26:13 +0300

    change in custom_queue (выделение в отдельный файл)

--- datastruct/custom_queue.py
+++ datastruct/custom_queue.py
@@ -1,8 +1,4 @@
-class Node:
-    def __init__(self, data, next_node=None):
-        """Инициализация узла"""
-        self.data = data
-        self.next_node = next_node
+from datastruct.stack import Node
 
 
 class Queue:
